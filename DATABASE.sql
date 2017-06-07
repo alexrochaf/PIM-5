7520b64e83588bf4d2945d278f65f0d3a9efccaf
Alex Rocha <alexrocha.f@outlook.com>
2017-06-07 01:02:00 -0400

    Configurando banco do Azure

--- DATABASE.sql
+++ DATABASE.sql
@@ -1,19 +1,15 @@
-drop database THEHOST
-go
-
-create database THEHOST
-go
-
-use THEHOST
-go
-
-create table Hospedes (
+create table Hospede (
 	Id  UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY not null,
 	Nome varchar(100) not null,
 	Celular varchar(20) not null,
 	Telefone varchar(20) null,
 	CPF varchar(14) not null,
-	Idade date not null
+	DataNascimento date not NULL,
+    Rua varchar(255) NOT NULL,
+	Numero INT NOT NULL,
+	Complemento VARCHAR(255) NULL,
+	Estado_Id INT FOREIGN KEY REFERENCES Estado(Id) NOT NULL,
+	Municipio INT FOREIGN KEY REFERENCES Municipio(Id) NOT NULL,
 );
 
 create table TipoQuarto (
@@ -24,7 +20,7 @@ create table TipoQuarto (
 )
 
 
-create table Quartos (
+create table Quarto (
 	Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	Tipo INT FOREIGN KEY REFERENCES TipoQuarto(Id) not null,
 	Numero int not null,
@@ -32,13 +28,13 @@ create table Quartos (
 	Estatus int not null,
 );
 
-create table Hospedagens (
-Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+create table Hospedagen (
+	Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	Entrada date not null,
 	Saida date null,
 	Estatus int not null,
-	Hospede UNIQUEIDENTIFIER FOREIGN KEY REFERENCES Hospedes(Id) not null,
-	Quarto int foreign key references Quartos(id) not null,
+	Hospede UNIQUEIDENTIFIER FOREIGN KEY REFERENCES Hospede(Id) not null,
+	Quarto int foreign key references Quarto(id) not null,
 	Valor decimal null,
 );
 
@@ -52,19 +48,13 @@ INSERT INTO TipoQuarto (Descricao, Camas, Valor) VALUES ('Double + Single Room (
 INSERT INTO TipoQuarto (Descricao, Camas, Valor) VALUES ('Double + Twin Room (Casal + 2)', 01, 120.00)
 
 --Inserindo Quartos
-INSERT INTO QUARTOS (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(01 , 01 ,01, 0);
-INSERT INTO QUARTOS (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(02 , 01 ,01, 0);
-INSERT INTO QUARTOS (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(03 , 01 ,03, 0);
-INSERT INTO QUARTOS (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(04 , 01 ,04, 0);
-INSERT INTO QUARTOS (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(05 , 01 ,03, 0);
-INSERT INTO QUARTOS (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(06 , 02 ,05, 0);
-INSERT INTO QUARTOS (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(07 , 02 ,07, 0);
-INSERT INTO QUARTOS (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(08 , 02 ,05, 0);
-INSERT INTO QUARTOS (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(09 , 02 ,07, 0);
-INSERT INTO QUARTOS (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(10 , 02 ,06, 0);
-
---Inserindo Clientes
-INSERT INTO HOSPEDES(NOME, CPF, Celular, Telefone, Idade) VALUES ('Alexsandro Rocha Fernandes', '024.944.592.14', '(69)9.9212-0077','(69)3228-4566','09/04/1994')
-INSERT INTO HOSPEDES (NOME, CPF, Celular, Idade) VALUES ('Fernando Augusto Ferreira', '024.914.232.24', '(69)9.8232-3237', '09/04/1994')
-INSERT INTO HOSPEDES (NOME, CPF, Celular, Idade) VALUES ('Ricardo Ferreira Mendes', '021.344.222.24', '(69)9.9932-3237', '09/04/1994')
-
+INSERT INTO QUARTO (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(01 , 01 ,01, 0);
+INSERT INTO QUARTO (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(02 , 01 ,01, 0);
+INSERT INTO QUARTO (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(03 , 01 ,03, 0);
+INSERT INTO QUARTO (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(04 , 01 ,04, 0);
+INSERT INTO QUARTO (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(05 , 01 ,03, 0);
+INSERT INTO QUARTO (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(06 , 02 ,05, 0);
+INSERT INTO QUARTO (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(07 , 02 ,07, 0);
+INSERT INTO QUARTO (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(08 , 02 ,05, 0);
+INSERT INTO QUARTO (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(09 , 02 ,07, 0);
+INSERT INTO QUARTO (NUMERO,ANDAR,Tipo,ESTATUS) VALUES(10 , 02 ,06, 0);
