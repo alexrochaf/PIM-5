f14204f60a1b640d57f8424c8c1ef7a47fe8e612
Alex Rocha <alexrocha.f@outlook.com>
2017-06-23 21:58:07 -0300

    Mudando as consultas no Banco de dados.

---
+++ js/select.js
@@ -0,0 +1,34 @@
+﻿//Cidades e Estado
+document.querySelector('#estado').addEventListener('change', (event) => {
+    var select = event.target;
+
+    var buscaCidades = "/Ajax/cidades?uf=" + select.options[select.selectedIndex].value
+
+    var cidades = [];
+
+    var selectCidades = document.querySelector("#cidade");
+
+    var load = $('.form-control');
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cidades = JSON.parse(this.responseText);
+            var x = document.querySelector("#cidade").options
+            x.length = 0
+
+            for (var i = 0; i < cidades.length; i++) {
+                var novaOpt = document.createElement("option");
+                novaOpt.value = cidades[i].Id;
+                novaOpt.textContent = cidades[i].Nome;
+                selectCidades.appendChild(novaOpt);
+            }
+
+            load.selectpicker('refresh');
+        }
+    };
+    xmlhttp.open("GET", buscaCidades, true);
+    xmlhttp.send();
+})
+
+//
